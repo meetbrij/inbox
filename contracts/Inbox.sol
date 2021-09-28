af2b23564e6a732cee0b109132f914ca18b3c683
brijesh <meetbrij@gmail.com>
2021-09-28 08:03:08 +0800

    updated code to recent

--- contracts/Inbox.sol
+++ contracts/Inbox.sol
@@ -1,13 +1,18 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.4.17;
 
 contract Inbox {
     string public message;
-    
-    function Inbox(string initialMessage) public {
+
+    constructor(string memory initialMessage) {
         message = initialMessage;
     }
-    
-    function setMessage(string newMessage) public {
+
+    function getMessage() public view returns(string memory) {
+        return message;
+    }
+
+    function setMessage(string memory newMessage) public {
         message = newMessage;
     }
 }
